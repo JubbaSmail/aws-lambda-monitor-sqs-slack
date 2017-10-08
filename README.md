01245d896bc08cc48c81d91e75f682077652ce68
Ismail-AlJubbah <imljh@live.com>
2017-10-08 12:37:39 +0200

    first push

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aws-lambda-monitor-sqs-slack
 AWS Lambda function to monitor SQS queue to a Slack channel
+
+https://learn.getgrav.org/advanced/yaml
+
+brew install aws-cli
+
+aws sqs list-queues
+
+sqs send-message --queue-url https://eu-west-1.queue.amazonaws.com/223381404055/edited_houses --message-body "Information about the largest city in Any Region."  --message-attributes file://msg.json
+
+aws sqs get-queue-attributes --queue-url https://eu-west-1.queue.amazonaws.com/223381404055/edited_houses  --attribute-names ApproximateNumberOfMessages ApproximateNumberOfMessagesDelayed ApproximateNumberOfMessagesNotVisible
--- index.js
+++ index.js
@@ -1,7 +1,126 @@
-brew install aws-cli
 
-aws sqs list-queues
+var http = require('http');
+var ymal = require('yamljs');
+var fs = require('fs');
 
-sqs send-message --queue-url https://eu-west-1.queue.amazonaws.com/223381404055/edited_houses --message-body "Information about the largest city in Any Region."  --message-attributes file://msg.json
+console.log('[Amazon CloudWatch Notification]');
 
-aws sqs get-queue-attributes --queue-url https://eu-west-1.queue.amazonaws.com/223381404055/edited_houses  --attribute-names ApproximateNumberOfMessages ApproximateNumberOfMessagesDelayed ApproximateNumberOfMessagesNotVisible
+/*
+ configuration for each condition.
+ add any conditions here
+*/
+// var ALARM_CONFIG = [
+// 	{
+// 		condition: "OK",
+// 		channel: "#cloudwatch",
+// 		mention: "<@here>",
+// 		color: "#21ff9f",
+// 		severity: "INFO"
+// 	},
+// 	{
+// 		condition: "ALARM",
+// 		channel: "#cloudwatch",
+// 		mention: "<@here>",
+// 		color: "#F35A00",
+// 		severity: "CRITICAL"
+// 	}
+	
+// ];
+
+// var SLACK_CONFIG = {
+// 	path: "/YOUR_PATH",
+// };
+
+var http = require ('https');
+var querystring = require ('querystring');
+exports.handler = function(event, context) {
+
+    var file = fs.createWriteStream("/tmp/queues.yaml");
+    var request = http.get("https://raw.githubusercontent.com/Ismail-AlJubbah/aws-lambda-monitor-sqs-slack/master/queues.ymal", function(response) {
+      response.pipe(file);
+      nativeObject = ymal.load('/tmp/queues.yaml');
+      console.log(nativeObject);
+      context.done(null, 'done!');
+    });
+
+
+	//console.log(event.Records[0]);
+
+	// parse information
+	// var message = event.Records[0].Sns.Message;
+	// var subject = event.Records[0].Sns.Subject;
+	// var timestamp = event.Records[0].Sns.Timestamp;
+
+	// // vars for final message
+	// var channel;
+	// var severity;
+	// var color;
+
+	// // create post message
+	// var s1 = subject.split(' ');
+	// var s2 = s1[1].split('awsroute53');
+	// var alarmMessage = "`"+s2[0]+"`";
+
+	// // check subject for condition
+	// for (var i=0; i < ALARM_CONFIG.length; i++) {
+	// 	var row = ALARM_CONFIG[i];
+	// 	console.log(row);
+	// 	if (subject.match(row.condition)) {
+	// 		console.log("Matched condition: "+row.condition);
+    //         		if(row.condition=="ALARM")
+	// 		    alarmMessage = row.mention+": "+alarmMessage+" "+" is not responding for the last 5 mins.\n";
+	// 		else if(row.condition=="OK")
+	// 		    alarmMessage = row.mention+": "+alarmMessage+" "+" is up now.\n";
+	// 		else
+	// 		    alarmMessage = alarmMessage;
+	// 		channel = row.channel;
+	// 		severity = row.severity;
+	// 		color = row.color;
+	// 		break;
+	// 	}
+	// }
+
+	// if (!channel) {
+	// 	console.log("Could not find condition.");
+	// 	context.done('error', "Invalid condition");
+	// }
+
+	// var payloadStr = JSON.stringify({
+	// "attachments": [
+	// 	{
+	// 		"fallback": alarmMessage,
+	// 		"text": alarmMessage,
+	// 		"mrkdwn_in": ["text"],
+	// 		"username": "AWS-CloudWatch",
+	// 		"color": color
+	// 	}
+	// ],
+	// 	"channel":channel
+	// });
+	// var postData = querystring.stringify({
+	//   "payload": payloadStr
+	// });
+	// console.log(postData);
+	// var options = {
+	// 	hostname: "hooks.slack.com",
+	// 	port: 443,
+	// 	path: SLACK_CONFIG.path,
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Type': 'application/x-www-form-urlencoded',
+	// 		'Content-Length': postData.length
+	// 	}
+	// };
+
+	// var req = http.request(options, function(res) {
+	// 	console.log("Got response: " + res.statusCode);
+	// 	res.on("data", function(chunk) {
+	// 		console.log('BODY: '+chunk);
+	// 		context.done(null, 'done!');
+	// 	});
+	// }).on('error', function(e) {
+	// 	context.done('error', e);
+	// });
+	// req.write(postData);
+	// req.end();
+};
